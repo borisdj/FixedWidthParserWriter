f6ff173b29b4bf02cd45ad35cf32c5a1df51d22e
borisdj <djurdjevicboris@hotmail.com>
2018-11-18 17:13:07 +0100

    ConfigUtil

--- FixedWidthParserWriter/ConfigUtil.cs
+++ FixedWidthParserWriter/ConfigUtil.cs
@@ -1,24 +1,11 @@
 ﻿namespace FixedWidthParserWriter
 {
-    public static class TypeText
-    {
-        public static string Int32 => nameof(System.Int32);
-        public static string Int64 => nameof(System.Int64);
-        public static string Decimal => nameof(System.Decimal);
-        public static string Single => nameof(System.Single);
-        public static string Double => nameof(System.Double);
-        public static string Boolean => nameof(System.Decimal);
-        public static string DateTime => nameof(System.DateTime);
-        public static string String => nameof(System.DateTime);
-        public static string Char => nameof(System.DateTime);
-    }
-
     public class DefaultConfig
     {
         // Formats
         public virtual string FormatNumberInteger { get; set; } = "0";
         public virtual string FormatNumberDecimal { get; set; } = "0.00";
-        public virtual string FormatBoolean { get; set; } = "T; ;F";
+        public virtual string FormatBoolean { get; set; } = "1; ;0";
         public virtual string FormatDateTime { get; set; } = "yyyyMMdd";
 
         //public virtual string FormatInt32 { get; set; }   // FormatNumberInteger
@@ -35,16 +22,16 @@
         public virtual PadSide PadSideNonNumeric { get; set; } = PadSide.Right;
     }
 
-    public enum FieldType
-    {
-        LineField,
-        FileField
-    }
-
     public enum PadSide
     {
         Default,
         Right,
         Left
+    }
+
+    public enum FieldType
+    {
+        LineField,
+        FileField
     }
 }
