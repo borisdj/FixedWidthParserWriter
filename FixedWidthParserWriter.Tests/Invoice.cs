7ba80444f4d89467307211b6c2e05b6f71b79a3e
borisdj <djurdjevicboris@hotmail.com>
2018-11-18 18:17:29 +0100

    NuGet 1.0.1

--- FixedWidthParserWriter.Tests/Invoice.cs
+++ FixedWidthParserWriter.Tests/Invoice.cs
@@ -7,7 +7,7 @@ namespace FixedWidthParserWriter.Tests
         [FixedWidthFileField(Line = 1)]
         public string CompanyName { get; set; }
 
-        // Format set on class with custom DefaultFormat so not required on each Attribute of DateTime Property
+        // Format set on class with FormatDateTime so not required on each Attribute of DateTime Property
         [FixedWidthFileField(Line = 4, Start = 15, Length = 19/*, Format = "yyyy-MM-dd"*/)]
         public DateTime Date { get; set; }
 
